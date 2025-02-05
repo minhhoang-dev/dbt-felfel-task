49688c6f81ad5f4943948d4989ae704171873df4
hoangnym <hoangnym94@gmail.com>
2025-02-05 06:27:39 +0000

    add filtering on product name

--- analyses/task_3_inventory_snapshot.sql
+++ analyses/task_3_inventory_snapshot.sql
@@ -6,5 +6,6 @@ select
 from {{ ref('fct_inventory_events') }}
 where event_at <= '2024-10-08'
     --- and location_name = '' --- if you want to filter by location_name
+    --- and product_name = '' -- if you want to filter by product_name
 qualify row_number() over (partition by location_name, product_name order by event_at desc) = 1 -- get the current stock given a certain event_at
 order by location_name, product_name, event_at desc
